0901f7c80c4c882d6e62f608067e5354c38f7be6
toby-cs1 <thammond@seattleu.edu>
2024-02-23 11:41:02 -0800

    working build

--- 1430/labs/l8/triangle.cpp
+++ 1430/labs/l8/triangle.cpp
@@ -8,7 +8,7 @@
 char triangleType(int side1, int side2, int side3)
 {
   // Check for invalid triangle
-  if (side1 + side2 < side3) {
+  if ((side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)) {
     return 'n';
   }
 
@@ -18,7 +18,7 @@ char triangleType(int side1, int side2, int side3)
   }
 
   // Check for isosceles triangle
-  if ((side1 == side2) || (side2 == side3)) {
+  if ((side1 == side2) || (side2 == side3) || (side1 == side3)) {
     return 'i';
   }
 
