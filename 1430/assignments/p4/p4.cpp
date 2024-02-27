10f02d7172cca955ed31347d12ba78a7e31cf175
Toby Hammond <tobiasjhammond@gmail.com>
2024-02-26 16:52:53 -0800

    Working build

--- 1430/assignments/p4/p4.cpp
+++ 1430/assignments/p4/p4.cpp
@@ -14,11 +14,9 @@ using namespace std;
 
 const int INITIAL_NUM_CHARS = 20;
 const int NEW_NUM_CHARS = 30;
-const int SHORT_SLEEP = 100;
-const int LONG_SLEEP = 1000;
+const int SHORT_SLEEP = 50;
+const int LONG_SLEEP = 1200;
 
-void removeChars(Queue& q, int reps);
-void removeChars(Stack& s, int reps);
 void print(Stack& s);
 void print(Queue& q);
 
@@ -37,75 +35,76 @@ int main()
     moreValues[i] = distribute(g);
 
   // Tests
-  // cout << endl
-  //      << "----------------------------------"
-  //      << "STACK TESTS"
-  //      << "----------------------------------" << endl;
+  cout << endl
+       << "----------------------------------"
+       << "STACK TESTS"
+       << "----------------------------------" << endl;
 
-  // cout << "Creating Stack s1" << endl;
-  // Stack s1;
-  // SLEEP(400);
-  // cout << "Populating s1 with 25 random chars: ";
-  // for (int i = 0; i < INITIAL_NUM_CHARS; i++) {
-  //   cout << initialValues[i] << " ";
-  //   s1.push(initialValues[i]);
-  // }
-  // SLEEP(LONG_SLEEP);
-  // cout << endl << "Making new stack s2 from s1" << endl;
-  // Stack s2(s1);
-  // SLEEP(LONG_SLEEP);
-  // cout << "Popping all values from s2:" << endl;
-  // removeChars(s2, INITIAL_NUM_CHARS);
-  // SLEEP(LONG_SLEEP);
-  // cout << "Attempting to pop one more value from s2:" << endl;
-  // SLEEP(LONG_SLEEP);
-  // try {
-  //   cout << s2.pop();
-  // } catch (Stack::emptyStackException) {
-  //   cout << "Error: Tried to pop off am empty stack" << endl;
-  // }
-  // SLEEP(LONG_SLEEP);
-  // cout << "Populating s2 with 30 new random chars: " << endl;
-  // for (int i = 0; i < NEW_NUM_CHARS; i++) {
-  //   cout << moreValues[i] << " ";
-  //   s2.push(moreValues[i]);
-  // }
-  // SLEEP(LONG_SLEEP);
-  // cout << endl << "Copying s2 to s1" << endl;
-  // s1 = s2;
-  // SLEEP(LONG_SLEEP);
-  // cout << "Popping values from both stacks until empty" << endl;
-  // cout << "s2:" << endl;
+  cout << "Creating Stack s1" << endl;
+  Stack s1;
+  SLEEP(400);
+  cout << "Populating s1 with 25 random chars: ";
+  for (int i = 0; i < INITIAL_NUM_CHARS; i++) {
+    cout << initialValues[i] << " ";
+    s1.push(initialValues[i]);
+  }
+  SLEEP(LONG_SLEEP);
+  cout << endl << "Making new stack s2 from s1" << endl;
+  Stack s2(s1);
+  SLEEP(LONG_SLEEP);
+  cout << "Popping all values from s2:" << endl;
+  print(s2);
+  SLEEP(LONG_SLEEP);
+  cout << "Attempting to pop one more value from s2:" << endl;
+  SLEEP(LONG_SLEEP);
+  try {
+    cout << s2.pop();
+  } catch (Stack::emptyStackException) {
+    cout << "Error: Tried to pop off am empty stack" << endl;
+  }
+  SLEEP(LONG_SLEEP);
+  cout << "Populating s2 with 30 new random chars: " << endl;
+  for (int i = 0; i < NEW_NUM_CHARS; i++) {
+    cout << moreValues[i] << " ";
+    s2.push(moreValues[i]);
+  }
+  SLEEP(LONG_SLEEP);
+  cout << endl << "Copying s2 to s1" << endl;
+  s1 = s2;
+  SLEEP(LONG_SLEEP);
+  cout << "Popping values from both stacks until empty" << endl;
+  cout << "s2:" << endl;
   string chars;
   char c;
-  // while (!s2.isEmpty()) {
-  //   c = s2.pop();
-  //   chars += c;
-  //   chars += ' ';
-  //   cout << "\r" << chars << flush << endl << "\33[1A";
-  //   SLEEP(SHORT_SLEEP);
-  // }
-  // cout << chars << endl;
-  // chars.clear();
-  // cout << "s1:" << endl;
-  // while (!s1.isEmpty()) {
-  //   c = s1.pop();
-  //   chars += c;
-  //   chars += ' ';
-  //   cout << "\r" << chars << flush << endl << "\33[1A";
-  //   SLEEP(SHORT_SLEEP);
-  // }
-  // cout << chars << endl;
-  // chars.clear();
-  // SLEEP(LONG_SLEEP);
+  while (!s2.isEmpty()) {
+    c = s2.pop();
+    chars += c;
+    chars += ' ';
+    cout << "\r" << chars << flush << endl << "\33[1A";
+    SLEEP(SHORT_SLEEP);
+  }
+  cout << chars << endl;
+  chars.clear();
+  cout << "s1:" << endl;
+  while (!s1.isEmpty()) {
+    c = s1.pop();
+    chars += c;
+    chars += ' ';
+    cout << "\r" << chars << flush << endl << "\33[1A";
+    SLEEP(SHORT_SLEEP);
+  }
+  cout << chars << endl;
+  chars.clear();
+  SLEEP(LONG_SLEEP);
 
-  // // Refreshing pools of chars
-  // for (int i = 0; i < INITIAL_NUM_CHARS; i++)
-  //   initialValues[i] = distribute(g);
-  // for (int i = 0; i < NEW_NUM_CHARS; i++)
-  //   moreValues[i] = distribute(g);
+  // Refreshing pools of chars
+  for (int i = 0; i < INITIAL_NUM_CHARS; i++)
+    initialValues[i] = distribute(g);
+  for (int i = 0; i < NEW_NUM_CHARS; i++)
+    moreValues[i] = distribute(g);
 
   cout << endl
+       << endl
        << "----------------------------------"
        << "QUEUE TESTS"
        << "----------------------------------" << endl;
@@ -124,7 +123,6 @@ int main()
   SLEEP(LONG_SLEEP);
   cout << "Dequeueing all values from q2:" << endl;
   print(q2);
-  // removeChars(q2, INITIAL_NUM_CHARS);
   SLEEP(LONG_SLEEP);
   cout << "Attempting to dequeue one more value from q2:" << endl;
   SLEEP(LONG_SLEEP);
@@ -165,42 +163,6 @@ int main()
     SLEEP(SHORT_SLEEP);
   }
   cout << chars << endl;
-}
-
-void removeChars(Queue& q, int reps)
-{
-  string chars;
-  char c;
-  try {
-    for (int i = 0; i < reps; i++) {
-      c = q.dequeue();
-      chars += c;
-      chars += ' ';
-      cout << "\r" << chars << flush << endl << "\33[1A";
-      SLEEP(SHORT_SLEEP);
-    }
-    cout << chars << endl;
-  } catch (Queue::emptyQueueException) {
-    cout << endl << "Error: tried to dequeue from an empty queue" << endl;
-  }
-}
-
-void removeChars(Stack& s, int reps)
-{
-  string chars;
-  char c;
-  try {
-    for (int i = 0; i < reps; i++) {
-      c = s.pop();
-      chars += c;
-      chars += ' ';
-      cout << "\r" << chars << flush << endl << "\33[1A";
-      SLEEP(SHORT_SLEEP);
-    }
-    cout << chars << endl;
-  } catch (Stack::emptyStackException) {
-    cout << endl << "Error: Tried to pop from an empty stack" << endl;
-  }
 }
 
 void print(Stack& s)
