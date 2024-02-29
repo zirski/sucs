4f87309dcfbe1f439370064edcc33759065ab15e
Toby Hammond <tobiasjhammond@gmail.com>
2024-02-28 16:39:47 -0800

    working build (mac)

--- 1430/assignments/p5/p5x/p5x.cpp
+++ 1430/assignments/p5/p5x/p5x.cpp
@@ -1,4 +1,9 @@
-// Useful libraries to include
+// Toby Hammond
+// p5.cpp
+// Purpose: Palindrome detector. Takes a user-submitted filepath, and for each
+// line of text in this file determines whether the line is a palindrome.
+// Returns the list of lines, with palindromes noted and highlighted in green.
+
 #include <cctype>
 #include <fstream>
 #include <iostream>
@@ -9,31 +14,40 @@
 
 using namespace std;
 
+// isPal will return true if text is an empty string. Technically this makes
+// sense...
 bool isPal(string text);
 string sanitizeLine(string line);
+// Pre: in needs to be a valid basic_istream
+void processLines(ifstream& in);
 
 int main()
 {
-  ifstream in("/home/fac/sreeder/class/cs1430/p5x.dat");
-  if (!in) {
-    cerr << "Error opening file" << endl;
-    return 1;
-  }
+  cout << "Welcome! this program takes a file of words/phrases and determines "
+          "whether they"
+       << endl
+       << "are palindromes." << endl;
 
-  string line;
-  int numPhrases = 0, numPals = 0;
-  while (getline(in, line) && !line.empty()) {
-    if (isPal(sanitizeLine(line))) {
-      cout << "\"\033[32m" << line << "\033[37m\" is a palindrome." << endl;
-      numPals++;
-    } else {
-      cout << "\"" << line << "\" is not a palindrome." << endl;
+  string finput;
+  char playAgain;
+
+  do {
+    cout << "Please enter a filepath: ";
+    cin >> finput;
+    ifstream in(finput);
+    if (!in) {
+      perror("Error");
+      return 1;
     }
-    numPhrases++;
-  }
 
-  cout << endl << "Number of words read: " << numPhrases << endl;
-  cout << "Number of palindromes found: " << numPals << endl;
+    processLines(in);
+    in.close();
+
+    cout << endl << "Evaluate another file? (y/n): ";
+    cin >> playAgain;
+  } while (playAgain != 'n' && playAgain != 'N');
+
+  cout << "Goodbye!" << endl;
   return 0;
 }
 
@@ -41,6 +55,7 @@ bool isPal(string text)
 {
   Queue forward;
   Stack reverse;
+  bool match = true;
 
   for (size_t i = 0; i < text.length(); i++) {
     char c = tolower(text[i]);
@@ -48,7 +63,6 @@ bool isPal(string text)
     reverse.push(c);
   }
 
-  bool match = true;
   while (!forward.isEmpty())
     if (forward.dequeue() != reverse.pop())
       match = false;
@@ -61,4 +75,24 @@ string sanitizeLine(string line)
     if (isspace(line.at(i)) || ispunct(line.at(i)))
       line.erase(i, 1);
   return line;
+}
+
+void processLines(ifstream& in)
+{
+  string line;
+  int numPhrases = 0, numPals = 0;
+
+  cout << endl;
+  while (getline(in, line) && !line.empty()) {
+    if (isPal(sanitizeLine(line))) {
+      cout << "\"\033[32m" << line << "\033[37m\" is a palindrome." << endl;
+      numPals++;
+    } else {
+      cout << "\"" << line << "\" is not a palindrome." << endl;
+    }
+    numPhrases++;
+  }
+
+  cout << endl << "Number of phrases read: " << numPhrases << endl;
+  cout << "Number of palindromes found: " << numPals << endl;
 }
