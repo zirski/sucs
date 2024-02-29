dac5a2eaa18ecff47c11466ba969e57300968f2c
Toby Hammond <tobiasjhammond@gmail.com>
2024-02-28 16:13:48 -0800

    rubric-adhering working build (mac)

--- 1430/assignments/p5/p5.cpp
+++ 1430/assignments/p5/p5.cpp
@@ -1,4 +1,3 @@
-// Useful libraries to include
 #include <cctype>
 #include <fstream>
 #include <iostream>
@@ -9,31 +8,38 @@
 
 using namespace std;
 
+// isPal will return true if word is an empty string. Technically this makes
+// sense...
 bool isPal(string word);
+// in needs to be a valid basic_istream
+void processWords(ifstream& in);
 
 int main()
 {
-  ifstream in("/home/fac/sreeder/class/cs1430/p5.dat");
-  // ifstream in("p5.dat");
-  if (!in) {
-    cerr << "Error opening file" << endl;
-    return 1;
-  }
+  string finput;
+  char playAgain;
 
-  string word;
-  int numWords = 0, numPals = 0;
-  while (in >> word) {
-    if (isPal(word)) {
-      cout << word << " is a palindrome." << endl;
-      numPals++;
-    } else {
-      cout << word << " is not a palindrome." << endl;
+  cout << "Welcome! this program takes a file of words and determines whether "
+          "they are "
+       << endl
+       << "palindromes." << endl;
+
+  do {
+    cout << "Please enter a filepath: ";
+    cin >> finput;
+    // ifstream in("/home/fac/sreeder/class/cs1430/p5x.dat");
+    ifstream in(finput);
+    if (!in) {
+      cerr << "Error: " << strerror(errno) << endl;
+      return 1;
     }
-    numWords++;
-  }
+    processWords(in);
+    in.close();
+    cout << endl << "Evaluate another file? (y/n): ";
+    cin >> playAgain;
+  } while (playAgain != 'n' && playAgain != 'N');
 
-  cout << endl << "Number of words read: " << numWords << endl;
-  cout << "Number of palindromes found: " << numPals << endl;
+  cout << "Goodbye!" << endl;
   return 0;
 }
 
@@ -41,6 +47,7 @@ bool isPal(string word)
 {
   Queue forward;
   Stack reverse;
+  bool match = true;
 
   for (size_t i = 0; i < word.length(); i++) {
     char c = tolower(word[i]);
@@ -48,9 +55,27 @@ bool isPal(string word)
     reverse.push(c);
   }
 
-  bool match = true;
   while (!forward.isEmpty())
     if (forward.dequeue() != reverse.pop())
       match = false;
   return match;
+}
+
+void processWords(ifstream& in)
+{
+  string word;
+  int numWords = 0, numPals = 0;
+
+  cout << endl;
+  while (in >> word) {
+    if (isPal(word)) {
+      cout << "\033[32m" << word << "\033[37m is a palindrome." << endl;
+      numPals++;
+    } else {
+      cout << word << " is not a palindrome." << endl;
+    }
+    numWords++;
+  }
+  cout << endl << "Number of words read: " << numWords << endl;
+  cout << "Number of palindromes found: " << numPals << endl;
 }
