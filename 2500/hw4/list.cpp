9f2894e1e90578fff7c8dc15ad67959024236f2b
Toby Hammond <tobiasjhammond@gmail.com>
2024-11-12 16:57:29 -0800

    working build

--- 2500/hw4/list.cpp
+++ 2500/hw4/list.cpp
@@ -14,52 +14,46 @@ int search(Node* head, int val);
 int main()
 {
   Node* root = nullptr;
+  int menuInput, value;
+  cout << "Integer Linked List" << endl;
+  cout << "Please enter an option:" << endl;
+  do {
+    cout << "1. Add Integer to Front" << endl;
+    cout << "2. Add Integer to Back" << endl;
+    cout << "3. Search For a Value" << endl;
+    cout << "4. Quit" << endl << ":";
+    cin >> menuInput;
 
-  root = addFront(root, 3);
-  root = addFront(root, 5);
+    switch (menuInput) {
+      case 1:
+        cout << "Enter value: ";
+        cin >> value;
+        root = addFront(root, value);
+        cout << "Done!" << endl;
+        break;
+      case 2:
+        cout << "Enter value: ";
+        cin >> value;
+        root = addEnd(root, value);
+        cout << "Done!" << endl;
+        break;
+      case 3:
+        cout << "Enter search value: ";
+        cin >> value;
+        int index = search(root, value);
+        if (index == 0)
+          cout << "value not found" << endl;
+        else
+          cout << "value is at node " << index << endl;
+    }
+  } while (menuInput != 4);
 
-  cout << search(root, 3);
-  // Node* root = nullptr;
-  // int menuInput, value;
-  // cout << "Integer Linked List" << endl;
-  // cout << "Please enter an option:" << endl;
-  // do {
-  //   cout << "1. Add Integer to Front" << endl;
-  //   cout << "2. Add Integer to Back" << endl;
-  //   cout << "3. Search For a Value" << endl;
-  //   cout << "4. Quit" << endl << ":";
-  //   cin >> menuInput;
-
-  //   switch (menuInput) {
-  //     case 1:
-  //       cout << "Enter value: ";
-  //       cin >> value;
-  //       root = addFront(root, value);
-  //       cout << "Done!" << endl;
-  //       break;
-  //     case 2:
-  //       cout << "Enter value: ";
-  //       cin >> value;
-  //       root = addEnd(root, value);
-  //       cout << "Done!" << endl;
-  //       break;
-  //     case 3:
-  //       cout << "Enter search value: ";
-  //       cin >> value;
-  //       int index = search(root, value);
-  //       if (index == 0)
-  //         cout << "value not found" << endl;
-  //       else
-  //         cout << "value is at node " << index << endl;
-  //   }
-  // } while (menuInput != 4);
-
-  // while (root != nullptr) {
-  //   cout << root->data << " ";
-  //   root = root->next;
-  // }
-  // cout << endl;
-  // return 0;
+  while (root != nullptr) {
+    cout << root->data << " ";
+    root = root->next;
+  }
+  cout << endl;
+  return 0;
 }
 
 Node* addFront(Node* head, int val)
